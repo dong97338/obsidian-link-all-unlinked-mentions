33504202f2f3afbf21780543c76174338b885a90
dong97338 <dong97338@gmail.com>
2023-12-31 19:57:28 +0900

    feat!: initialize main.ts
    - function iu(prefix, word, suffix): Determine if a word contains specified prefix and suffix for URL or link inclusion.
    
    - m.style.backgroundColor: Set background color to red for words with excluded from being linked.
    
    - let b=document.createElement('button'): Create button that links all unlinked mentions without the excluded.
    
    Refs: #6 #7 #8

--- main.ts
+++ main.ts
@@ -1,134 +1,56 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { Plugin } from 'obsidian';
+function iu(prefix, word, suffix) {
+    const combinedString = prefix + word + suffix;
+    const urlRegex = /https?:\/\/[^\s]+/g;
 
-// Remember to rename these classes and interfaces!
+    let match;
+    while ((match = urlRegex.exec(combinedString)) !== null) {
+        const url = match[0];
+        const urlStartIndex = match.index;
+        const urlEndIndex = urlStartIndex + url.length;
 
-interface MyPluginSettings {
-	mySetting: string;
+        // check word index
+        const wordStartIndex = prefix.length;
+        const wordEndIndex = prefix.length + word.length;
+
+        if (wordStartIndex >= urlStartIndex && wordEndIndex <= urlEndIndex) {
+            return true; // word in URL
+        }
+    }
+	if(prefix?.replace(/\[.*?\]/g, '').includes('[')) // word in link
+		return true;
+	if(suffix?.replace(/\[\[.*?\]\]/g, '').replace(/\[.*?\]/g, '').includes(']'))
+		return true;
+
+    return false; // word not in URL nor link
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
-
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
-
-	async onload() {
-		await this.loadSettings();
-
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
+export default class extends Plugin {
+	onload() {
+		this.interval = setInterval(() =>document.querySelectorAll('.backlink-pane').forEach(p=>{
+			p.children[3].querySelectorAll('.search-result-file-match').forEach(m=>{
+				let a=m.querySelector('.search-result-file-matched-text');//alias
+				if(iu(a.previousSibling?.innerHTML,a.innerText,a.nextSibling?.innerText))
+					m.style.backgroundColor = 'rgba(255, 0, 0, 0.1)';
+			})
+			if(p.children[2].childElementCount<4){
+				let b=document.createElement('button');
+				b.classList.add('link-all');
+				b.textContent = 'Link all';
+				console.log('ck');
+				b.addEventListener('click', e=>{
+					e.stopPropagation();
+					p.children[3].querySelectorAll('.search-result-file-match').forEach(m=>{
+						let a=m.querySelector('.search-result-file-matched-text');
+						if(!iu(a.previousSibling?.innerHTML,a.innerText,a.nextSibling?.innerText))
+							m.querySelector('.search-result-file-match-replace-button').click();
+					})
+				},{capture: true});
+				p.children[2].appendChild(b);
 			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+		}),500);
 	}
-
 	onunload() {
-
+		clearInterval(this.interval);
 	}
-
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
-
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
-}
-
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
-
-	display(): void {
-		const {containerEl} = this;
-
-		containerEl.empty();
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
-	}
-}
+};
